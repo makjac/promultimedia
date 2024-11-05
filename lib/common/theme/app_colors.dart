b58ef05412a5eaa38f1830e945327f47eec13254
makjac <maks.jackowski@proton.me>
2024-11-05 23:42:56 +0100

    feat: add `AppColors` class

---
+++ lib/common/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF3d6ea6);
+
+  static const Color elevatedButtonBorderColor = Colors.white;
+  static const Color elevatedButtonBackgroundColor = Color(0XFF0767b0);
+  static const Color elevatedButtonForegroundColor = Colors.white;
+}
