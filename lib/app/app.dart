7213110d52ef5ab16fb490658bca1b716ce97bff
makjac <maks.jackowski@proton.me>
2024-11-05 15:56:34 +0100

    refactor: update `App` to use `MaterialApp.router` for enhanced navigation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:promultimedia/router/app_router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp.router(
+      title: 'Pro Multimedia',
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: AppRouter.router.routeInformationParser,
+      routeInformationProvider: AppRouter.router.routeInformationProvider,
+      routerDelegate: AppRouter.router.routerDelegate,
+    );
   }
 }
