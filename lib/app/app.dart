2c5d019f6ac6442cd90d394605f6fa30a1396ddf
makjac <maks.jackowski@proton.me>
2024-11-05 23:45:49 +0100

    feat: update app theme and adjust router import path

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:promultimedia/router/app_router.dart';
+import 'package:promultimedia/common/router/app_router.dart';
+import 'package:promultimedia/common/theme/app_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,10 +9,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Pro Multimedia',
-      debugShowCheckedModeBanner: false,
+      theme: AppTheme.theme,
       routeInformationParser: AppRouter.router.routeInformationParser,
       routeInformationProvider: AppRouter.router.routeInformationProvider,
       routerDelegate: AppRouter.router.routerDelegate,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
